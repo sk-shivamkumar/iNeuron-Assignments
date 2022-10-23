05c00d35aec0dcda418536c651b701a4b63834b9
Shivam Kumar <96992199+sk-shivamkumar@users.noreply.github.com>
2022-10-24 01:24:19 +0530

    Update Q1.java

--- Assignment-1/Q1.java
+++ Assignment-1/Q1.java
@@ -63,7 +63,7 @@ public class Q1 {
 				System.out.print(" ");
 			}
 			
-//	R-->
+// R-->
 			for(int j=0; j<n; j++) {
 				if(j==0 || i==0&&j<n-1 || i==(n-1)/2&&j<n-1 || j==n-1&&i!=0&&i<(n-1)/2 || i==j&&i>(n-1)/2){
 					System.out.print("*");

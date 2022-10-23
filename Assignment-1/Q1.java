a06fbe52d49921fcbf03302b83cb2dd3814a4a30
Shivam Kumar <96992199+sk-shivamkumar@users.noreply.github.com>
2022-10-24 01:23:29 +0530

    Add files via upload

---
+++ Assignment-1/Q1.java
@@ -0,0 +1,108 @@
+package assignment1;
+
+public class Q1 {
+
+	public static void main(String[] args) {
+
+		int n = 10;
+		
+// I-->
+		for(int i=0; i<n; i++) {
+			for(int j=0; j<n; j++) {
+				if(i==0 || i==n-1 || j==(n-1)/2) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			
+			for(int j=0; j<1; j++) {
+				System.out.print(" ");
+			}
+		
+// N-->			
+			for(int j=0; j<n; j++) {
+				if(j==0 || j==n-1 || i==j) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			
+			for(int j=0; j<1; j++) {
+				System.out.print(" ");
+			}
+			
+// E-->
+			for(int j=0; j<n; j++) {
+				if(j==0 || i==0 || i==(n-1)/2 || i==n-1) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			
+			for(int j=0; j<1; j++) {
+				System.out.print(" ");
+			}
+			
+// U-->			
+			for(int j=0; j<n; j++) {
+				if(j==0&&i<n-1 || j==n-1&&i<n-1 || i==n-1&&j!=0&&j!=n-1) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			
+			for(int j=0; j<1; j++) {
+				System.out.print(" ");
+			}
+			
+//	R-->
+			for(int j=0; j<n; j++) {
+				if(j==0 || i==0&&j<n-1 || i==(n-1)/2&&j<n-1 || j==n-1&&i!=0&&i<(n-1)/2 || i==j&&i>(n-1)/2){
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			
+			for(int j=0; j<1; j++) {
+				System.out.print(" ");
+			}
+			
+// O-->
+			for(int j=0; j<n; j++) {
+				if(i==0&&j!=0&&j<n-1 || i==n-1&&j!=0&&j<n-1 || j==0&&i!=0&&i!=n-1 || j==n-1&&i!=0&&i<n-1){
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			
+			for(int j=0; j<1; j++) {
+				System.out.print(" ");
+			}
+
+// N-->
+			for(int j=0; j<n; j++) {
+				if(j==0 || j==n-1 || i==j) {
+					System.out.print("*");
+				}
+				else {
+					System.out.print(" ");
+				}
+			}
+			
+			
+			System.out.println();
+		}
+	}
+}
